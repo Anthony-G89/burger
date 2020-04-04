5bfae064e54ae8f01616ad3f089369e577b1f970
Anthony Guerrero <adguerrero1935@gmail.com>
2020-04-04 19:25:04 -0400

    working on front End JS

---
+++ public/js/frontEndBurgers.js
@@ -0,0 +1,41 @@
+$(function(){
+    $(".addBurger").on("submit", event =>{
+        event.preventDefault();
+
+        const newBurger ={
+            burgerName: $("userInput").val().trim()
+        };
+
+        $.ajax("/api/burgers",{
+            type: "POST",
+            data: newBurger
+        }).then(function(){
+            console.log("New Burger Created");
+
+            location.reload();
+            
+        });
+    });
+
+    $(".deleteBurgers").on("click", event =>{
+        const id = $(this).data("id");
+
+        $.ajax("/api/burgers" + id,{
+            type: "DELETE"
+        }).then(
+            function(){
+                console.log("deleted burger", id);
+
+                location.reload();
+                
+            }
+        );
+    });
+
+
+
+
+
+
+
+});
