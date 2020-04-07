f011c332e96e1a920f781e23bdea8c5fffdf7d78
Anthony Guerrero <adguerrero1935@gmail.com>
2020-04-07 19:48:43 -0400

    added jawDB code

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,25 @@
 var mysql = require("mysql");
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else{
-    
-var connection = mysql.createConnection({
-    host: "localhost",
-    PORT: 3306,
-    user: "root",
-    password: "Agar112405",
-    database: "burger_db"
-});
+} else {
+
+    var connection = mysql.createConnection({
+        host: "localhost",
+        PORT: 3306,
+        user: "root",
+        password: "Agar112405",
+        database: "burger_db"
+    });
 };
 
-connection.connect(err =>{
-    if(err){
+connection.connect(err => {
+    if (err) {
         console.error("error connecting:" + err.stack);
         return;
     }
     console.log("connecting as id" + connection.threadId);
-    
+
 });
 
 connection.connect();
